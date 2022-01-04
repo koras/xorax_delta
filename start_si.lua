9d3ba4f9f6e956fe9cc2ec99492c35dda4c91c9d
koras <kos6476@yandex.ru>
2022-01-04 19:17:41 +0300

    Перевел настройки в отдельный класс

--- start_si.lua
+++ start_si.lua
@@ -1,22 +1,36 @@
 
 -- Доллар/рубль
+script_path = getScriptPath()
 
-dofile(getScriptPath() .. "\\setting\\path.lua");
-dofile(getScriptPath() .. "\\setting\\work_si.lua");
-dofile(getScriptPath() .. "\\setting\\engine.lua");
-dofile(getScriptPath() .. "\\modules\\start_engine.lua");
+local script_path = getScriptPath()
+dofile(script_path .. "\\setting\\path.lua");
+ 
 
+local Setting=  dofile(script_path .. "\\setting\\work_si.lua");
+ 
+
+local EngineClass = dofile(script_path .. "\\modules\\start_engine.lua");
+local Loger = dofile(script_path .. "\\engine\\loger.lua");
+
+
+
+
+ -- message(Setting.version .. ' =  ' .. Setting.SEC_CODE.. Setting.test())
+ 
+
+
+ 
+local Log = Loger:new(Setting);
+local Engine = EngineClass:new(Setting, Log);
 
 function OnInit()
-    EngineInit(setting)
+    Engine.EngineInit()
 end
- 
- 
+
 
 function main()
-    EngineMain(setting)
+    Engine:EngineMain()
 end
- 
 
 -- https://quikluacsharp.ru/quik-qlua/primer-prostogo-torgovogo-dvizhka-simple-engine-qlua-lua/
 
@@ -24,27 +38,27 @@ end
 -- The function is called by the terminal when information on the request comes from the server
     -- http://luaq.ru/OnOrder.html
 function OnOrder(order)
-    EngineOrder(order)
+    Engine:EngineOrder(order)
 end
 
 -- OnTransReply -> OnTrade -> OnOrder 
 -- The function is called by the terminal when information on the deal comes from the server
 function OnTrade(trade)
-    EngineOnTrade(trade, setting)
+    Engine:EngineOnTrade(trade)
 end
  
 
 -- The function is called by the terminal when information on the deal comes from the server
 function OnStopOrder(trade)
-    EngineStopOrder(trade)
+    Engine:EngineStopOrder(trade)
 end
 
  
 function OnTransReply(trans_reply) 
-    EngineTransReply(trans_reply, setting)
+    Engine:EngineTransReply(trans_reply)
 end
 
 -- This function is called when the script is stopped
 function OnStop()
-    EngineStop()
+    Engine:EngineStop()
 end
